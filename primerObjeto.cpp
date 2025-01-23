1c0ff20865d9d96cbb21e27afdbcdd6344560d86
JGprogramacion <jgprogramacion23@gmail.com>
2025-01-22 23:11:27 -0500

    Finalizado primer objeto

--- primerObjeto.cpp
+++ primerObjeto.cpp
@@ -1,6 +1,26 @@
 #include<iostream>
+#include<string>
 using namespace std;
 
+class Persona {
+    //Propiedades de la Persona.
+    //Al utilizar la palabra public, se puede ingresar desde afuera al atributo.
+    public:
+        string nombre = "Jonathan";
+        int edad = 26;
+    //Acciones Por medio de funciones.
+    void saludar(){
+        cout<<nombre<<endl;
+    }
+};
+
+
 int main(){
-    return 0;
+    Persona p1 = Persona();
+    Persona p2 = Persona();
+
+    cout<<p1.edad<<endl;
+    p1.saludar();
+    p2.nombre = "David";
+    p2.saludar();
 }
